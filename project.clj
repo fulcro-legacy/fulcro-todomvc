3faadf275b206af6ca085f2aac72b0074f8219fc
Ethan Gracer <EDGracer@thenavisway.com>
2016-02-07 14:47:45 -0800

    Initial commit for untangled todomvc

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject untangled-todomvc "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.omcljs/om "1.0.0-alpha30"]
+                 [untangled-client "0.4.2-SNAPSHOT"]]
+  :plugins [[lein-figwheel "0.5.0-1"]]
+  :clean-targets ^{:protect false} ["resources/public/js" "target"]
+  :cljsbuild {:builds [{:id           "dev"
+                        :source-paths ["src"]
+                        :figwheel     true
+                        :compiler     {:main          "untangled-todomvc.core"
+                                       :asset-path    "js/compiled/dev"
+                                       :output-to     "resources/public/js/compiled/untangled-todomvc.js"
+                                       :output-dir    "resources/public/js/compiled/dev"
+                                       :optimizations :none}}]}
+  :figwheel {:css-dirs ["resources/public/css"]})
---
+++ src/untangled-todomvc/core.cljs
@@ -0,0 +1,3 @@
+(ns untangled-todomvc.core)
+
+(js/console.log "Untangled Todo with hot-code reload!")
