760baaaf4267161bea062144a464b7f974a5e079
Tony Kay <twkay@thenavisway.com>
2016-03-15 13:58:55 -0700

    removed dead code

--- src/server/todomvc/api.clj
+++ src/server/todomvc/api.clj
@@ -28,18 +28,6 @@
     {}
     omids->tempids))
 
-(defn replace-ref-types [dbc refs m]
-  "@dbc   the database to query
-   @refs  a set of keywords that ref datomic entities, which you want to access directly
-          (rather than retrieving the entity id)
-   @m     map returned from datomic pull containing the entity IDs you want to deref"
-  (clojure.walk/postwalk
-    (fn [arg]
-      (if (and (coll? arg) (refs (first arg)))
-        (update-in arg [1] (comp :db/ident (partial d/entity dbc) :db/id))
-        arg))
-    m))
-
 (defn make-list [connection list-name]
   (let [id (d/tempid :db.part/user)
         tx [{:db/id id :list/title list-name}]
