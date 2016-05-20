50b274129561051e9a867d89df1f17a50a86c03c
Tony Kay <twkay@thenavisway.com>
2016-05-20 12:12:37 -0700

    version change

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject untangled-todomvc "1.0.2"
+(defproject untangled-todomvc "1.0.3-SNAPSHOT"
   :description "TodoMVC implemention using untangled.client"
   :url "http://www.thenavisway.com/"
   :license {:name "MIT"
